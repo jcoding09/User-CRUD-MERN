3a1d2cc69367888914abe89bd172ff18df5cb1ca
jsloader <ruststatistics@gmail.com>
2024-03-18 00:49:28 +0530

    update dbconfig

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -4,11 +4,17 @@ const mongoose = require("mongoose");
 //ES7 method to connect with DB
 const dbConnect = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+    //const uri = process.env.MONGODB_URI;
+
+    const uri = "mongodb://localhost:27017/MERN_CRUD";
+
+    const options = {
       useNewUrlParser: true,
-      useFinadandModify: false,
-    });
-    console.log(`MongoDB database Connected : ${connect.connection.host}`);
+      useUnifiedTopology: true,
+    };
+
+    const conn = await mongoose.connect(uri, options);
+    console.log(`MongoDB database Connected : ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error connecting to MongoDB : ${error}`);
   }
